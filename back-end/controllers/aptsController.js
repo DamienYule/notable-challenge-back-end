049e368073d4f55ebca91ee00cf5653be53c3654
DamienYule <damienyule@gmail.com>
2022-07-21 19:22:38 -0400

    all routes working

--- back-end/controllers/aptsController.js
+++ back-end/controllers/aptsController.js
@@ -7,11 +7,14 @@ const {
   updateApt,
   deleteApt
 } = require("../queries/aptsQueries");
-
+const appointments_for_doc_at_time = async (doctor_id,date) =>{
+  const allApts = await fetchAllApts(doctor_id);
+  const filteredApt = allApts.payload.filter((apt)=>{return apt.date.toISOString().substring(0, date.length) == date})
+  return filteredApt 
+}
 appointments.get("/", async (req, res) => {
   const {doctor_id, date} = req.query
-  const allApts = await fetchAllApts(doctor_id);
-  const filteredApt = allApts.payload.filter((apt)=>{return apt.date.toISOString().substring(0, 10) == date})
+  const filteredApt = await appointments_for_doc_at_time(doctor_id,date)
   res.json(filteredApt);  
 });
 
@@ -22,13 +25,16 @@ appointments.get("/:id", async (req, res) => {
 });
 
 appointments.post("/", async (req, res) => {
-  const time = req.body.time
+  const {doctor_id, time, date} = req.body
   const min = parseInt(time.split(":")[1])
+  const filteredApt = await appointments_for_doc_at_time(doctor_id, date)
+  const existingAppts = filteredApt.filter((apt)=>{return apt.time == time})
   if(min%15 !== 0){
     res.json({ success: false, payload: "Not an interval of 15 min" })
-  }
-  else
-  {
+  } else if (existingAppts.length > 2) {
+    res.json({ success: false, payload: "Too many appointments at this time for this doctor" })
+  }  
+  else {
     const createdApt = await newApt(req.body);
     res.json(createdApt);
   }
